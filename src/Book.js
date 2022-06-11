4712c9391e0554e0053c05b6ff93bb7965b706bb
arcenyovia <arcenyovia@gmail.com>
2022-06-12 01:30:41 +0900

    booklist

--- src/Book.js
+++ src/Book.js
@@ -2,29 +2,18 @@ import React from 'react';
 
 const Book = props => {
 
-    const {content, id, modDate, regDate, title, writer} = props.value;
+	const { content, id, modDate, regDate, title, writer } = props.value;
 
-    return (
-        <div>
-            <li>
-                <ul>
-                    {content}
-                    <br/>
-                    {id}
-                    <br/>
-                    {modDate}
-                    <br/>
-                    {regDate}
-                    <br/>
-                    {title}
-                    <br/>
-                    {writer}
-                    <br/>
-
-                </ul>
-            </li>
-        </div>
-    );
+	return (
+		<tr>
+			<th>{id}</th>
+			<td>{title}</td>
+			<td>{content}</td>
+			<td>{writer}</td>
+			<td>{regDate}</td>
+			<td>{modDate}</td>
+		</tr>
+	);
 };
 
 export default Book;
--- src/BookList.js
+++ src/BookList.js
@@ -1,28 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import Book from "./Book";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Book from './Book';
 
 const BookList = () => {
 
-    const [bookList, setBookList] = useState([]);
+	const [bookList, setBookList] = useState([]);
 
-    useEffect(() => {
-        const getBookLists = async () => {
-            const book = await axios.get(
-                "guestbook/list/"
-            );
-            setBookList(book.data);
-        };
-        getBookLists();
-    }, [])
-    return (
-        <div>
-            {bookList.map((book) =>
-            <Book key={book.id}
-                value={book}/>
-            )}
-        </div>
-    );
+	useEffect(() => {
+		const getBookLists = async () => {
+			const book = await axios.get(
+				'guestbook/list/'
+			);
+			setBookList(book.data);
+		};
+		getBookLists();
+	}, []);
+	return (
+		<div>
+			<h1>Guest Page</h1>
+			<table className={"table"}>
+				<thead>
+				<tr>
+					<th>#</th>
+					<th>Id</th>
+					<th>Title</th>
+					<th>content</th>
+					<th>Created date</th>
+					<th>Updated date</th>
+				</tr>
+				</thead>
+				{bookList.map((book) =>
+					<Book key={book.id} value={book} />)}
+			</table>
+		</div>
+	);
 
 };
 
